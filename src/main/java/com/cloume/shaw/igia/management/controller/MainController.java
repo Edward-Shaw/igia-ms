92ecc21ef29f3fd895bce285c086f9689560abba
Edward-Shaw <yxiao@aliyun.com>
2016-09-10 21:58:07 +0800

    change file name.

--- src/main/java/com/cloume/shaw/igia/management/controller/MainController.java
+++ src/main/java/com/cloume/shaw/igia/management/controller/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
 	
 	@RequestMapping(value = "/index", method = {RequestMethod.GET})
-	public String index(Principal principal){
+	public String indexPage(Principal principal){
 		
 		/*
 		if(principal == null || principal.getName() == null){
@@ -19,5 +19,41 @@ public class MainController {
 		*/
 		
 		return "index";
+	}
+	
+	@RequestMapping(value = "/user", method = {RequestMethod.GET})
+	public String userPage(Principal principal){
+		
+		/*
+		if(principal == null || principal.getName() == null){
+			return "error";
+		}
+		*/
+		
+		return "user";
+	}
+	
+	@RequestMapping(value = "/subscribe", method = {RequestMethod.GET})
+	public String subscribePage(Principal principal){
+		
+		/*
+		if(principal == null || principal.getName() == null){
+			return "error";
+		}
+		*/
+		
+		return "subscribe";
+	}
+	
+	@RequestMapping(value = "/class", method = {RequestMethod.GET})
+	public String classPage(Principal principal){
+		
+		/*
+		if(principal == null || principal.getName() == null){
+			return "error";
+		}
+		*/
+		
+		return "class";
 	}
 }
