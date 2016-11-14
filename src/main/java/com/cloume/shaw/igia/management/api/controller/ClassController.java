493d607b238321d028e980a155085718002ed3f3
Edward-Shaw <yxiao@aliyun.com>
2016-11-14 19:42:46 +0800

    add class controller.

---
+++ src/main/java/com/cloume/shaw/igia/management/api/controller/ClassController.java
@@ -0,0 +1,10 @@
+package com.cloume.shaw.igia.management.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/class")
+public class ClassController {
+
+}
