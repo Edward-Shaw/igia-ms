8724e68b4c8c256de573bab5100139fa1e4aa3b3
Edward-Shaw <yxiao@aliyun.com>
2016-12-22 16:25:08 +0800

    add task service

---
+++ src/main/java/com/cloume/shaw/igia/management/iservice/ITaskService.java
@@ -0,0 +1,24 @@
+package com.cloume.shaw.igia.management.iservice;
+
+import java.util.List;
+
+import com.cloume.shaw.igia.common.resource.Task;
+
+public interface ITaskService {
+
+	/**
+	 * 根据筛选条件获取作业列表
+	 * @param state
+	 * @param course
+	 * @param createdTime
+	 * @param page
+	 * @return
+	 */
+	public List<Task> listByPage(String state, String course, String createdTime, int[] page);
+	
+	/**
+	 * 添加新的课程作业
+	 * @param task
+	 */
+	public void addNewTask(Task task);
+}
---
+++ src/main/java/com/cloume/shaw/igia/management/service/TaskService.java
@@ -0,0 +1,90 @@
+package com.cloume.shaw.igia.management.service;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+
+import com.cloume.shaw.igia.common.resource.Course;
+import com.cloume.shaw.igia.common.resource.Task;
+import com.cloume.shaw.igia.common.utils.Const;
+import com.cloume.shaw.igia.management.iservice.AbstractServiceBase;
+import com.cloume.shaw.igia.management.iservice.ICourseService;
+import com.cloume.shaw.igia.management.iservice.ITaskService;
+
+public class TaskService extends AbstractServiceBase implements ITaskService {
+
+	@Override
+	public void addNewTask(Task task) {
+		
+		getMongoTemplate().save(task);;
+	}
+
+	@Override
+	public List<Task> listByPage(String state, String course, String createdTime, int[] page) {
+		if(page == null){
+			page = new int[2];
+			page[0] = 0;
+			page[1] = 20;
+		}
+		
+		// 获取页码及每一页的长度
+		int pageNumber = page[0];
+		int pageSize = page[1];
+		
+		Query query = new Query();
+		Criteria criterion = new Criteria();
+		if(state.compareToIgnoreCase("default") != 0){
+			criterion.and("state").is(state);
+		}else{
+			criterion.and("state").ne(Const.STATE_DELETED);
+		}
+		
+		if(course.compareToIgnoreCase("default") != 0){
+			criterion.and("course").is(course);
+		}
+		
+		if (createdTime.compareToIgnoreCase("default") != 0) {
+			Calendar now = Calendar.getInstance();
+			now.set(Calendar.HOUR_OF_DAY, 0);
+			now.set(Calendar.SECOND, 0);
+			now.set(Calendar.MINUTE, 0);
+			now.set(Calendar.MILLISECOND, 0);
+			long datetime;
+			switch (createdTime) {
+			case "today":
+				datetime = now.getTimeInMillis();
+				criterion = criterion.and("createTime").gte(datetime);
+				break;
+			case "yesterday":
+				datetime = now.getTimeInMillis();
+				now.add(Calendar.DATE, -1);
+				long datetime1 = now.getTimeInMillis();
+				criterion = criterion.and("createTime").gte(datetime1).lte(datetime);
+				break;
+			case "this_week":
+				now.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+				datetime = now.getTimeInMillis();
+				criterion = criterion.and("createTime").gte(datetime);
+				break;
+			case "this_month":
+				now.set(Calendar.DAY_OF_MONTH, 1);
+				datetime = now.getTimeInMillis();
+				criterion = criterion.and("createTime").gte(datetime);
+				break;
+			}
+		}
+
+		// 查询"state"不等于"STATE_DELETED"的预约，跳过前 pageNumber*pageSize条记录，返回之后的pageSize条记录
+		query = Query.query(criterion).skip(pageNumber * pageSize).limit(pageSize);
+
+		// 根据预约id倒序排列
+		List<Task> taskList = getMongoTemplate().find(query.with(new Sort(Direction.DESC, "_id")), Task.class);
+		
+		return taskList;
+	}
+
+}
