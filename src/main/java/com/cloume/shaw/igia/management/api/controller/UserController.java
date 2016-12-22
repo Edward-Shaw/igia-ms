2d1ae94ff75488b6c86b4af1ab87ea476a6a7b09
Edward-Shaw <yxiao@aliyun.com>
2016-12-22 11:17:34 +0800

    create user

--- src/main/java/com/cloume/shaw/igia/management/api/controller/UserController.java
+++ src/main/java/com/cloume/shaw/igia/management/api/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.cloume.shaw.igia.management.api.controller;
 
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cloume.shaw.igia.common.controller.AbstractController;
 import com.cloume.shaw.igia.common.resource.User;
 import com.cloume.shaw.igia.common.rest.RestResponse;
+import com.cloume.shaw.igia.common.utils.Const;
 import com.cloume.shaw.igia.common.utils.Updater;
 import com.cloume.shaw.igia.management.iservice.IUserService;
 import com.cloume.shaw.igia.management.repository.UserRepository;
@@ -33,6 +36,16 @@ public class UserController extends AbstractController{
 	@Autowired
 	private UserRepository userRepository;
 	
+	final String[] FIELDS = new String[] {"name", "address", "mobile", "email"};
+	
+	/**
+	 * 获取用户列表
+	 * @param request
+	 * @param page
+	 * @param state
+	 * @param time
+	 * @return
+	 */
 	@RequestMapping(method = RequestMethod.GET)
 	public PagingRestResponse<User> list(HttpServletRequest request, 
 			@RequestParam(value = "page", required = false, defaultValue = "0, 20") int[] page,
@@ -44,6 +57,12 @@ public class UserController extends AbstractController{
 		return new PagingRestResponse<User>(0, "OK", users);
 	}
 	
+	/**
+	 * 获取用户信息详情
+	 * @param request
+	 * @param id
+	 * @return
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public RestResponse<User> getUserDetailById(HttpServletRequest request,
 			@PathVariable("id") String id){
@@ -52,7 +71,14 @@ public class UserController extends AbstractController{
 		return new RestResponse<User>(0, "OK", user);
 	}
 	
-	@RequestMapping(value = "/{id}", method = {RequestMethod.POST, RequestMethod.PUT})
+	/**
+	 * 修改用户信息
+	 * @param request
+	 * @param id
+	 * @param body
+	 * @return
+	 */
+	@RequestMapping(value = "/{id}", method = {RequestMethod.POST})
 	public RestResponse<User> updateUserById(HttpServletRequest request,
 			@PathVariable("id") String id,
 			@RequestBody Map<String, Object> body){
@@ -61,15 +87,72 @@ public class UserController extends AbstractController{
 			return RestResponse.bad(-1, "user not found!");
 		}
 		
-		if(body.get("banned").toString().compareToIgnoreCase("true") == 0){
-			body.replace("banned", true);
-		}else{
-			body.replace("banned", false);
-		}
-		
-		user = new Updater<User>(user).update(body);
+		user = new Updater<User>(user).update(body, (key, value) ->{
+			if(key.compareToIgnoreCase("banned") == 0){
+				if(value.toString().compareToIgnoreCase("true") == 0){
+					value = true;
+				}else{
+					value = false;
+				}
+			}
+			return value;
+		});
 		user = userRepository.save(user);
 		
 		return new RestResponse<User>(0, "Ok", user);
+	}
+	
+	/**
+	 * 创建新的用户
+	 * @param request
+	 * @param body
+	 * @return
+	 */
+	@RequestMapping(method = {RequestMethod.PUT})
+	public RestResponse<User> createUser(HttpServletRequest request,
+			@RequestBody Map<String, Object> body){
+		for (String field : FIELDS) {
+			if (!body.containsKey(field)) {
+				return RestResponse.bad(-10001, String.format("missed property %s", field), null);
+			}
+		}
+		
+		User user = new User(new ObjectId().toString());
+		user = new Updater<User>(user).update(body, (key, value)->{
+			if(key.compareToIgnoreCase("banned") == 0){
+				if(value.toString().compareToIgnoreCase("true") == 0){
+					value = true;
+				}else{
+					value = false;
+				}
+			}
+			return value;
+		});
+		user.setCode(generateUserCode());
+		user = userRepository.save(user);
+		
+		return RestResponse.good(user);
+	}
+	
+	/**
+	 * code = YYMM(年月)-用户数量序号(位数根据数据库中实际用户数量进行动态变更)
+	 * 
+	 * @return
+	 */
+	private String generateUserCode() {
+		Calendar now = Calendar.getInstance();
+		String time = (now.get(Calendar.YEAR) + "").substring(2) + String.format("%02d", now.get(Calendar.MONTH) + 1);
+		final String prefix = time;
+		final String pattern = prefix + "-\\d{1,5}";
+
+		String code = "";
+		synchronized (this) {
+			long count = getMongoTemplate().count(
+					Query.query(Criteria.where("code").regex(pattern).and("state").ne(Const.STATE_DELETED)), User.class,
+					"order") + 1;
+			code = prefix + "-" + count;
+		}
+
+		return code;
 	}
 }
--- src/main/java/com/cloume/shaw/igia/management/controller/MainController.java
+++ src/main/java/com/cloume/shaw/igia/management/controller/MainController.java
@@ -130,4 +130,13 @@ public class MainController {
 	public String login() {
 		return "login";
 	}
+	
+	/**
+	 * 作业管理
+	 * @return
+	 */
+	@RequestMapping("/task")
+	public String task(){
+		return "task";
+	}
 }
