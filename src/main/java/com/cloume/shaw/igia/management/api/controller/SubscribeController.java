be4b269823fefc80cfbefe3a1cd4dd5ceca7c643
Edward-Shaw <yxiao@aliyun.com>
2017-01-02 21:58:10 +0800

    modify susbscribe.

--- src/main/java/com/cloume/shaw/igia/management/api/controller/SubscribeController.java
+++ src/main/java/com/cloume/shaw/igia/management/api/controller/SubscribeController.java
@@ -1,7 +1,13 @@
 package com.cloume.shaw.igia.management.api.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +16,7 @@ import com.cloume.shaw.igia.common.controller.AbstractController;
 import com.cloume.shaw.igia.common.resource.Subscribe;
 import com.cloume.shaw.igia.common.rest.RestResponse;
 import com.cloume.shaw.igia.common.utils.Const;
+import com.cloume.shaw.igia.common.utils.Updater;
 import com.cloume.shaw.igia.management.repository.SubscribeRepository;
 
 /**
@@ -55,6 +62,46 @@ public class SubscribeController extends AbstractController {
 		subscribe.setState(Const.STATE_DELETED);
 		subscribeRepository.save(subscribe);
 		
+		return RestResponse.good(subscribe);
+	}
+	
+	/**
+	 * update Subscribe by code.
+	 * @param code
+	 * @param body
+	 * @return
+	 */
+	@RequestMapping(value = "/{code}", method = RequestMethod.POST)
+	public RestResponse<Subscribe> updateSubscribeByCode(@PathVariable("code") String code,
+			@RequestBody Map<String, Object> body){
+		Subscribe subscribe = subscribeRepository.findSubscirbeByCode(code);
+		if(subscribe == null){
+			return RestResponse.bad(-1, "subscribe not found: " + code);
+		}
+		
+		System.out.println(body);
+		for(String key : body.keySet()){
+			if(key.compareToIgnoreCase("courses") == 0){
+				String[] courses = body.get("courses").toString().split("  ");
+				ArrayList<String> list = new ArrayList<String>(Arrays.asList(courses));
+				subscribe.setCourses(list);
+			}else if(key.compareToIgnoreCase("username") == 0){
+				subscribe.getUser().setUsername(body.get("username").toString());
+			}else if(key.compareToIgnoreCase("mobile") == 0){
+				subscribe.getUser().setMobile(body.get("mobile").toString());
+			}else if(key.compareToIgnoreCase("address") == 0){
+				subscribe.getUser().setAddress(body.get("address").toString());
+			}else if(key.compareToIgnoreCase("subscribeClass") == 0){
+				subscribe.setSubscribeClass(body.get("subscribeClass").toString());
+			}else if(key.compareToIgnoreCase("state") == 0){
+				subscribe.setState(body.get("state").toString());
+			}else{
+				System.out.println("Wrong key: " + key);
+			}
+		}
+		
+		subscribeRepository.save(subscribe);
+
 		return RestResponse.good(subscribe);
 	}
 
